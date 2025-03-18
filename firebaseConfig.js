d263eba72fe4c012de744b8067697ad3d9d124b7
MANASWINI VOOLAPALLI <91537487+Manaswini123456@users.noreply.github.com>
2025-03-18 17:51:32 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,12 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBQpEVPObvIokKsw5Qk782NH3R0xwfRCxM",
-  authDomain: "bugbyte-b2796.firebaseapp.com",
-  projectId: "bugbyte-b2796",
-  storageBucket: "bugbyte-b2796.appspot.com",
-  messagingSenderId: "712193416928",
-  appId: "1:712193416928:web:1350e6084fe9d0f23dba85"
+  
 };
 
 // Initialize Firebase
